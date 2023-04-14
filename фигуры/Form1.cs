1952a15c5b1def86b9f2ecd98d964f09f7f6b31a
Илья Тупицин <ilhatupitsin@yandex.ru>
2023-04-14 20:46:13 +0300

    ну ка

--- фигуры/Form1.cs
+++ фигуры/Form1.cs
@@ -5,10 +5,28 @@ namespace фигуры
         public Form1()
         {
             InitializeComponent();
-
+            Paint += Form1_Paint;
         }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Random r = new Random();
+            for (int i = 0, coordinate = 0, j = 10, b = 20, h = 400, w = 12; i < 10; i++, j += 20, b += 20, h -= 10, w += 12)
+            {
+                coordinate = r.Next(10, 200);
+                Pen blackPen = new Pen(Color.Black, 1);
+                Pen pen = new Pen(Color.FromArgb(r.Next(0, 254), r.Next(0, 254), r.Next(0, 254)));
+                e.Graphics.DrawEllipse(pen, r.Next(0, 700), r.Next(0, 300), coordinate, coordinate);
 
-        private void Form1_Load(object sender, EventArgs e)
+                e.Graphics.DrawLine(blackPen, 50, 250, 50, 75);
+                e.Graphics.DrawLine(blackPen, 250, 250, 250, 75);
+                e.Graphics.DrawEllipse(blackPen, 50, j, 200, 125);
+
+                e.Graphics.DrawLine(blackPen, 309, 250, 405, 2);
+                e.Graphics.DrawLine(blackPen, 510, 250, 415, 2);
+                e.Graphics.DrawEllipse(blackPen, h, j, b, w);
+            }
+        }
+            private void Form1_Load(object sender, EventArgs e)
         {
 
         }
